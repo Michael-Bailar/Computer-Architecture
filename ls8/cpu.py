0fb3c0691f960258e9a45fb0003cd2d29d6df1dd
Michael-B1764 <mikebailar@gmail.com>
2020-08-18 18:53:27 -0700

    Finished with day 2 repeat

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -27,22 +27,27 @@ class CPU:
         """Run the CPU."""
         self.running = True
 
-
-
+        # command list (assuming this will change)
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
+        MUL = 0b10100010
 
         while self.running:
 
             self.IR = self.RAM[self.PC]
             self.MAR = self.PC
+
+            # update operand a
             self.MAR = self.PC + 1
             self.ram_read() # MDR Becomes the thing at ram[MAR]
             operand_a = self.MDR
+
+            # update operand b
             self.MAR = self.PC + 2
-            self.ram_read() # MDR Becomes the thing at ram[MAR]
+            self.ram_read()
             operand_b = self.MDR
+
             if self.IR == HLT:
                 self.running = False
             elif self.IR == PRN:
@@ -51,39 +56,56 @@ class CPU:
             elif self.IR == LDI:
                 self.REG[operand_a] = operand_b
                 self.PC += 3
+            elif self.IR == MUL:
+                self.alu("MUL", operand_a, operand_b) 
+                self.PC += 3
             else:
-                print("I don't know that command")
+                print("Ended due to unknown command: self.IR == ?")
+                self.running = False
 
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
+        # # For now, we've just hardcoded a program:
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
+        # program = [
+        #     # From print8.ls8
             
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.RAM[address] = instruction
-            address += 1
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    comment_split = line.split("#")
+                    n = comment_split[0].strip()
 
+                    if n == '':
+                        continue
+
+                    val = int(n, 2)
+                    self.RAM[address] = val
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {filename} not found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.REG[reg_a] += self.REG[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.REG[reg_a] -= self.REG[reg_b]
+        elif op == "MUL":
+            self.REG[reg_a] = self.REG[reg_a] * self.REG[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
